d1abd209f879aca91a0b9f7c87216fd83d8ffe60
Matt Hill <MattDHill@users.noreply.github.com>
2021-01-22 15:42:33 -0700

    Update docker_entrypoint.sh

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -10,14 +10,14 @@ if [ ! -f /root/filebrowser.db ]; then
     echo 'data:' >> /root/start9/stats.yaml
     echo '  - name: Default Username' >> /root/start9/stats.yaml
     echo '    value: admin' >> /root/start9/stats.yaml
-    echo '    description: ~' >> /root/start9/stats.yaml
+    echo '    description: This is your default username. While it is not necessary, you may change it inside your File Browser web application. That change, however, will not be reflected here. If you change your default username and forget your new username, there is no way to regain access.' >> /root/start9/stats.yaml
     echo '    copyable: true' >> /root/start9/stats.yaml
-    echo '    masked: true' >> /root/start9/stats.yaml
     echo '    qr: false' >> /root/start9/stats.yaml
     echo '  - name: Default Password' >> /root/start9/stats.yaml
     echo '    value: "'"$password"'"' >> /root/start9/stats.yaml
-    echo '    description: This is your randomly-generated, default password. While we DO NOT recommend changing it, you may do so inside your File Browser web application, but that change will not be reflected here. If you change the default password and forget your new password, there is no way to regain access.' >> /root/start9/stats.yaml
+    echo '    description: This is your randomly-generated, default password. While it is not necessary, you may change it inside your File Browser web application. That change, however, will not be reflected here. If you change your default password and forget your new password, there is no way to regain access.' >> /root/start9/stats.yaml
     echo '    copyable: true' >> /root/start9/stats.yaml
+    echo '    masked: true' >> /root/start9/stats.yaml
     echo '    qr: false' >> /root/start9/stats.yaml
     filebrowser users add admin $password --perm.admin=true
 fi
