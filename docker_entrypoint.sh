6d4a216e27d5b419fc0fc5eddbef67364083c8c4
Aiden McClelland <me@drbonez.dev>
2021-03-01 14:45:45 -0700

    add action to reset password

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -6,20 +6,41 @@ if [ ! -f /root/filebrowser.db ]; then
     filebrowser config init
     filebrowser config set --address=0.0.0.0 --port=8080 --root=/root/data
     password=$(cat /dev/urandom | base64 | head -c 16)
-    echo 'version: 1' > /root/start9/stats.yaml
+    echo 'version: 2' > /root/start9/stats.yaml
     echo 'data:' >> /root/start9/stats.yaml
-    echo '  - name: Default Username' >> /root/start9/stats.yaml
+    echo '  Default Username:' >> /root/start9/stats.yaml
     echo '    value: admin' >> /root/start9/stats.yaml
     echo '    description: This is your default username. While it is not necessary, you may change it inside your File Browser web application. That change, however, will not be reflected here. If you change your default username and forget your new username, there is no way to regain access.' >> /root/start9/stats.yaml
     echo '    copyable: true' >> /root/start9/stats.yaml
     echo '    qr: false' >> /root/start9/stats.yaml
-    echo '  - name: Default Password' >> /root/start9/stats.yaml
+    echo '  Default Password:' >> /root/start9/stats.yaml
     echo '    value: "'"$password"'"' >> /root/start9/stats.yaml
     echo '    description: This is your randomly-generated, default password. While it is not necessary, you may change it inside your File Browser web application. That change, however, will not be reflected here. If you change your default password and forget your new password, there is no way to regain access.' >> /root/start9/stats.yaml
     echo '    copyable: true' >> /root/start9/stats.yaml
     echo '    masked: true' >> /root/start9/stats.yaml
     echo '    qr: false' >> /root/start9/stats.yaml
-    filebrowser users add admin $password --perm.admin=true
+    filebrowser users add admin "$password" --perm.admin=true
+fi
+
+if [ "$1" = "reset-password" ]; then
+    password=$(cat /dev/urandom | base64 | head -c 16)
+    echo 'version: 2' > /root/start9/stats.yaml
+    echo 'data:' >> /root/start9/stats.yaml
+    echo '  Default Username:' >> /root/start9/stats.yaml
+    echo '    value: admin' >> /root/start9/stats.yaml
+    echo '    description: This is your default username. While it is not necessary, you may change it inside your File Browser web application. That change, however, will not be reflected here. If you change your default username and forget your new username, there is no way to regain access.' >> /root/start9/stats.yaml
+    echo '    copyable: true' >> /root/start9/stats.yaml
+    echo '    qr: false' >> /root/start9/stats.yaml
+    echo '  Default Password:' >> /root/start9/stats.yaml
+    echo '    value: "'"$password"'"' >> /root/start9/stats.yaml
+    echo '    description: This is your randomly-generated, default password. While it is not necessary, you may change it inside your File Browser web application. That change, however, will not be reflected here. If you change your default password and forget your new password, there is no way to regain access.' >> /root/start9/stats.yaml
+    echo '    copyable: true' >> /root/start9/stats.yaml
+    echo '    masked: true' >> /root/start9/stats.yaml
+    echo '    qr: false' >> /root/start9/stats.yaml
+    filebrowser users update admin -p "$password"
+    echo "Your new password is: $password"
+    echo "This will also be reflected in `Properties` for this service."
+    exit 0
 fi
 
 lighttpd -f /etc/lighttpd/httpd.conf
