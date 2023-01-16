0565b0e6d3eec73206cae8407997be444beb62c0
BluJ <mogulslayer@gmail.com>
2023-01-16 16:07:30 -0700

    chore: Add in the js backup

--- scripts/procedures/migration.ts
+++ scripts/procedures/migration.ts
@@ -2,6 +2,8 @@ import { migrations, types as T } from "../deps.ts";
 import { manifest } from "../generated/manifest.ts";
 
 export const migration: T.ExpectedExports.migration = migrations.fromMapping(
-  {},
-  manifest.version
+  {
+    /// No migrations for 2.22.4.2
+  },
+  manifest.version,
 );
