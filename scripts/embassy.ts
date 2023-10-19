a233a0e0fb558f4e8ba33c59c84965f708ddffe9
Mariusz Kogen <k0gen@pm.me>
2023-10-19 20:07:30 +0200

    Remove comment

--- scripts/embassy.ts
+++ scripts/embassy.ts
@@ -1,4 +1,3 @@
 export { properties } from "./procedures/properties.ts";
-//export { health } from "./procedures/health.ts";
 export { migration } from "./procedures/migration.ts";
 export { createBackup, restoreBackup } from "./procedures/backups.ts";
