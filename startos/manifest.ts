0b57b24e49e135af803f7b4a4dbb642451d0dd4b
Mariusz Kogen <k0gen@pm.me>
2025-09-03 20:04:30 +0200

    Makefile refactor

--- startos/manifest.ts
+++ startos/manifest.ts
@@ -1,4 +1,10 @@
 import { setupManifest } from '@start9labs/start-sdk'
+import { SDKImageInputSpec } from '@start9labs/start-sdk/base/lib/types/ManifestTypes'
+
+const BUILD = process.env.BUILD || ''
+
+const architectures =
+  BUILD === 'x86_64' || BUILD === 'aarch64' ? [BUILD] : ['x86_64', 'aarch64']
 
 export const manifest = setupManifest({
   id: 'filebrowser',
@@ -21,9 +27,12 @@ export const manifest = setupManifest({
       source: {
         dockerTag: 'filebrowser/filebrowser:v2.32.0',
       },
-    },
+      arch: architectures,
+    } as SDKImageInputSpec,
   },
-  hardwareRequirements: {},
+  hardwareRequirements: {
+    arch: architectures,
+  },
   alerts: {
     install: null,
     update: null,
