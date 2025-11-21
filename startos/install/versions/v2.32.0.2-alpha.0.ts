17df799d16f7c2b55d741cd3b730e6d0e4fd6add
Mariusz Kogen <k0gen@pm.me>
2025-11-21 18:42:10 +0100

    bump version tag for update

--- startos/install/versions/v2.32.0.2-alpha.0.ts
+++ startos/install/versions/v2.32.0.2-alpha.0.ts
@@ -1,7 +1,7 @@
 import { IMPOSSIBLE, VersionInfo } from '@start9labs/start-sdk'
 
 export const v_2_32_0_2 = VersionInfo.of({
-  version: '2.32.0:2-alpha.0',
+  version: '2.32.0:2-alpha.1',
   releaseNotes: `\
 Updated for StartOS 0.4.0
 
